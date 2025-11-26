30e567ddb6ad50aec641796a20fb45be46e5bcf8
dev-yugensys <developer@yugensys.com>
2025-11-26 19:25:19 +0530

    Auto Sroll enabled

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 import { ChatService } from './services/chat.service';
 
 @Component({
@@ -13,7 +13,13 @@ export class AppComponent {
   userInput: string = '';
   isLoading = false;
 
+  @ViewChild('chatBoxContainer') messagesContainer!: ElementRef;
+
   constructor(private chatService: ChatService) {}
+
+  ngAfterViewInit() {
+    this.scrollToBottom();
+  }
 
   sendMessage() {
     if (!this.userInput.trim() || this.isLoading) return;
@@ -21,16 +27,19 @@ export class AppComponent {
     const userText = this.userInput;
     this.userInput = '';
     this.messages.push({ sender: 'user', text: userText });
+    this.scrollToBottom();
 
     // call backend
     this.chatService.sendMessage(userText).subscribe({
       next: (response) => {
         this.messages.push({sender: 'ai', text: response.response});
         this.isLoading = false;
+        this.scrollToBottom();
       },
       error: (err) => {
         this.messages.push({sender: 'ai', text: "⚠️ Something went wrong!"});
         this.isLoading = false;
+        this.scrollToBottom();
       }
     });
   }
@@ -38,5 +47,14 @@ export class AppComponent {
   quickAsk(text: string) {
     this.userInput = text;
     this.sendMessage();
+  }
+
+  scrollToBottom() {
+    try {
+      setTimeout(() => {
+        const el = this.messagesContainer.nativeElement;
+        el.scrollTop = el.scrollHeight;
+      }, 50);
+    } catch {}
   }
 }
